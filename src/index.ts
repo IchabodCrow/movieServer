0461ddb15255e6e5604e07440b8ca1b822b25241
Nikolay <nikolay@maria-morozova.kodep.ru>
2020-10-09 11:34:08 +0300

    skeleton

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { createConnection } from "typeorm"
+import routes from "./routes";
+
+const app = express();
+
+createConnection()
+  .then((connection) => {})
+  .catch((error) => console.error(error));
+
+app.use(bodyParser.json());
+app.use(routes);
+app.listen(3333);
