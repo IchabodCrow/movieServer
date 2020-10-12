e5945bd844143fa5c8d09b0c7a00cc3bb3edbdfe
Nikolay <nikolay@maria-morozova.kodep.ru>
2020-10-12 13:49:54 +0300

    fix-conversation

--- src/services/queryTMDB.ts
+++ src/services/queryTMDB.ts
@@ -1,18 +1,31 @@
 const fetch = require('node-fetch');
-const apiKey = process.env.REACT_APP_API_KEY;
+const apiKey = process.env.API_KEY;
 const url = "https://api.themoviedb.org/3/";
 
-interface Data {
-  year?: number | string,
-  rating?: number | string,
+interface IFilterData {
+  year?: string,
+  rating?: string,
   genres?: Array<number>,
   page?: number
+}
+
+const genreFilter = (genres: Array<number>) => {
+  genres.length > 0 && 'with_genres=$' + genres.join()
+}
+const ratingFilter = (rating: string) => {
+  rating.length > 0 && 'vote_average.gte=' + rating 
+}
+const yearFilter = (year: string) => {
+  year.length > 0 && 'year=' + year
+}
+const pageNumb = (page: number) => {
+  page && 'page' + page
 }
 
 export const movieGenres = () => {
   return fetch(`${url}genre/movie/list?api_key=${apiKey}}`)
     .then(res => res.json())
-    .then(data => data)
+    .then(data =>  data)
     .catch(err => console.error(err));
 };
 
@@ -21,14 +34,9 @@ export const movieListWithFilters = ({
   rating = "",
   genres= [],
   page,
-}: Data) => {
-  const genre = genres.join();
-  const genreUrl = `with_genres=${genre}`;
-  const ratingUrl = `vote_average.gte=${rating}`;
-  const yearUrl = `year=${year}`;
-  const pageUrl = `page${page}`;
+}: IFilterData) => {
   return fetch(
-    `${url}discover/movie?api_key=${apiKey}&${pageUrl}&${yearUrl}&${ratingUrl}&${genreUrl}`
+    `${url}discover/movie?api_key=${apiKey}&${pageNumb(page)}&${yearFilter(year)}&${ratingFilter(rating)}&${genreFilter(genres)}`
   )
     .then((res) => res.json())
     .then((data) => data)
